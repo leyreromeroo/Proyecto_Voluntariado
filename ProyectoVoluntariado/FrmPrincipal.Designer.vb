bc3a272501d7d7d24f45287c4b4b690d56508bc3
Markel Alvarado <markel.alvarado@gmail.com>
2025-04-02 10:41:36 +0200

    Primer prototipo de diseño

--- ProyectoVoluntariado/FrmPrincipal.Designer.vb
+++ ProyectoVoluntariado/FrmPrincipal.Designer.vb
@@ -22,46 +22,383 @@ Partial Class FrmPrincipal
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        Me.pnlAnyadirVoluntariado = New System.Windows.Forms.Panel()
+        Me.Label21 = New System.Windows.Forms.Label()
+        Me.Label20 = New System.Windows.Forms.Label()
+        Me.Label19 = New System.Windows.Forms.Label()
+        Me.Label18 = New System.Windows.Forms.Label()
+        Me.Label17 = New System.Windows.Forms.Label()
+        Me.Label16 = New System.Windows.Forms.Label()
+        Me.Label15 = New System.Windows.Forms.Label()
+        Me.Label14 = New System.Windows.Forms.Label()
+        Me.Label13 = New System.Windows.Forms.Label()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.TextBox6 = New System.Windows.Forms.TextBox()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
         Me.Label1 = New System.Windows.Forms.Label()
-        Me.pnlAnyadirVoluntariado.SuspendLayout()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.LBL_NOMBRE_ACTIVIDAD = New System.Windows.Forms.Label()
+        Me.Label22 = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
-        'pnlAnyadirVoluntariado
+        'Label21
         '
-        Me.pnlAnyadirVoluntariado.BackColor = System.Drawing.SystemColors.ButtonHighlight
-        Me.pnlAnyadirVoluntariado.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
-        Me.pnlAnyadirVoluntariado.Controls.Add(Me.Label1)
-        Me.pnlAnyadirVoluntariado.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
-        Me.pnlAnyadirVoluntariado.Location = New System.Drawing.Point(32, 28)
-        Me.pnlAnyadirVoluntariado.Name = "pnlAnyadirVoluntariado"
-        Me.pnlAnyadirVoluntariado.Size = New System.Drawing.Size(743, 196)
-        Me.pnlAnyadirVoluntariado.TabIndex = 0
+        Me.Label21.AutoSize = True
+        Me.Label21.Location = New System.Drawing.Point(29, 141)
+        Me.Label21.Name = "Label21"
+        Me.Label21.Size = New System.Drawing.Size(76, 16)
+        Me.Label21.TabIndex = 59
+        Me.Label21.Text = "FECHA FIN"
+        '
+        'Label20
+        '
+        Me.Label20.AutoSize = True
+        Me.Label20.Location = New System.Drawing.Point(29, 113)
+        Me.Label20.Name = "Label20"
+        Me.Label20.Size = New System.Drawing.Size(93, 16)
+        Me.Label20.TabIndex = 58
+        Me.Label20.Text = "FECHA INICIO"
+        '
+        'Label19
+        '
+        Me.Label19.AutoSize = True
+        Me.Label19.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label19.Location = New System.Drawing.Point(107, 538)
+        Me.Label19.Name = "Label19"
+        Me.Label19.Size = New System.Drawing.Size(57, 36)
+        Me.Label19.TabIndex = 57
+        Me.Label19.Text = "2/7"
+        '
+        'Label18
+        '
+        Me.Label18.AutoSize = True
+        Me.Label18.Location = New System.Drawing.Point(14, 505)
+        Me.Label18.Name = "Label18"
+        Me.Label18.Size = New System.Drawing.Size(251, 16)
+        Me.Label18.TabIndex = 56
+        Me.Label18.Text = "CANTIDAD VOLUNTARIOS AÑADIDOS:"
+        '
+        'Label17
+        '
+        Me.Label17.AutoSize = True
+        Me.Label17.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label17.Location = New System.Drawing.Point(426, 218)
+        Me.Label17.Name = "Label17"
+        Me.Label17.Size = New System.Drawing.Size(32, 36)
+        Me.Label17.TabIndex = 55
+        Me.Label17.Text = "3"
+        '
+        'Label16
+        '
+        Me.Label16.AutoSize = True
+        Me.Label16.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label16.Location = New System.Drawing.Point(377, 218)
+        Me.Label16.Name = "Label16"
+        Me.Label16.Size = New System.Drawing.Size(32, 36)
+        Me.Label16.TabIndex = 54
+        Me.Label16.Text = "2"
+        '
+        'Label15
+        '
+        Me.Label15.AutoSize = True
+        Me.Label15.BackColor = System.Drawing.Color.LightGreen
+        Me.Label15.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label15.Location = New System.Drawing.Point(330, 217)
+        Me.Label15.Name = "Label15"
+        Me.Label15.Size = New System.Drawing.Size(32, 36)
+        Me.Label15.TabIndex = 53
+        Me.Label15.Text = "1"
+        '
+        'Label14
+        '
+        Me.Label14.AutoSize = True
+        Me.Label14.Location = New System.Drawing.Point(39, 315)
+        Me.Label14.Name = "Label14"
+        Me.Label14.Size = New System.Drawing.Size(137, 16)
+        Me.Label14.TabIndex = 52
+        Me.Label14.Text = "MARKEL ALVARADO"
+        '
+        'Label13
+        '
+        Me.Label13.AutoSize = True
+        Me.Label13.BackColor = System.Drawing.Color.PaleGreen
+        Me.Label13.Location = New System.Drawing.Point(39, 284)
+        Me.Label13.Name = "Label13"
+        Me.Label13.Size = New System.Drawing.Size(116, 16)
+        Me.Label13.TabIndex = 51
+        Me.Label13.Text = "DIEGO TEJEDOR"
+        '
+        'Label12
+        '
+        Me.Label12.AutoSize = True
+        Me.Label12.Location = New System.Drawing.Point(39, 247)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(109, 16)
+        Me.Label12.TabIndex = 50
+        Me.Label12.Text = "JAVIER MARTIN"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.BackColor = System.Drawing.Color.PaleGreen
+        Me.Label2.Location = New System.Drawing.Point(39, 216)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(104, 16)
+        Me.Label2.TabIndex = 49
+        Me.Label2.Text = "IKER SANCHEZ"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label9.Location = New System.Drawing.Point(426, 453)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(25, 36)
+        Me.Label9.TabIndex = 48
+        Me.Label9.Text = "-"
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label10.Location = New System.Drawing.Point(388, 453)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(32, 36)
+        Me.Label10.TabIndex = 47
+        Me.Label10.Text = "+"
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Location = New System.Drawing.Point(323, 180)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(226, 16)
+        Me.Label11.TabIndex = 46
+        Me.Label11.Text = "LISTA ALUMNOS CON MISMO TIPO"
+        '
+        'TextBox6
+        '
+        Me.TextBox6.Location = New System.Drawing.Point(312, 199)
+        Me.TextBox6.Multiline = True
+        Me.TextBox6.Name = "TextBox6"
+        Me.TextBox6.Size = New System.Drawing.Size(251, 251)
+        Me.TextBox6.TabIndex = 45
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label8.Location = New System.Drawing.Point(139, 453)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(25, 36)
+        Me.Label8.TabIndex = 44
+        Me.Label8.Text = "-"
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Location = New System.Drawing.Point(672, 504)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(292, 22)
+        Me.TextBox5.TabIndex = 43
+        Me.TextBox5.Text = "ESCRIBIR NOMBRE ACTIVIDAD"
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(672, 62)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(292, 22)
+        Me.TextBox4.TabIndex = 42
+        Me.TextBox4.Text = "ESCRIBIR NOMBRE ACTIVIDAD"
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(672, 400)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(292, 22)
+        Me.TextBox3.TabIndex = 41
+        Me.TextBox3.Text = "ESCRIBIR NOMBRE ACTIVIDAD"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(669, 475)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(143, 16)
+        Me.Label7.TabIndex = 40
+        Me.Label7.Text = "ELIMINAR ACTIVIDAD"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(669, 368)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(154, 16)
+        Me.Label6.TabIndex = 39
+        Me.Label6.Text = "MODIFICAR ACTIVIDAD"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(669, 30)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(164, 16)
+        Me.Label5.TabIndex = 38
+        Me.Label5.Text = "CONSULTAR ACTIVIDAD"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(83, 453)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(32, 36)
+        Me.Label4.TabIndex = 37
+        Me.Label4.Text = "+"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(39, 177)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(226, 16)
+        Me.Label3.TabIndex = 36
+        Me.Label3.Text = "LISTA ALUMNOS CON MISMO TIPO"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(27, 196)
+        Me.TextBox2.Multiline = True
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(251, 251)
+        Me.TextBox2.TabIndex = 35
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 16
+        Me.ListBox1.Location = New System.Drawing.Point(186, 80)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(291, 20)
+        Me.ListBox1.TabIndex = 34
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(38, 13)
+        Me.Label1.Location = New System.Drawing.Point(29, 84)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(229, 16)
-        Me.Label1.TabIndex = 1
-        Me.Label1.Text = "Gestión de Acciones de Voluntariado"
+        Me.Label1.Size = New System.Drawing.Size(112, 16)
+        Me.Label1.TabIndex = 33
+        Me.Label1.Text = "TIPO ACTIVIDAD"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(186, 24)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(292, 22)
+        Me.TextBox1.TabIndex = 32
+        '
+        'LBL_NOMBRE_ACTIVIDAD
+        '
+        Me.LBL_NOMBRE_ACTIVIDAD.AutoSize = True
+        Me.LBL_NOMBRE_ACTIVIDAD.Location = New System.Drawing.Point(29, 28)
+        Me.LBL_NOMBRE_ACTIVIDAD.Name = "LBL_NOMBRE_ACTIVIDAD"
+        Me.LBL_NOMBRE_ACTIVIDAD.Size = New System.Drawing.Size(140, 16)
+        Me.LBL_NOMBRE_ACTIVIDAD.TabIndex = 31
+        Me.LBL_NOMBRE_ACTIVIDAD.Text = "NOMBRE ACTIVIDAD"
+        '
+        'Label22
+        '
+        Me.Label22.AutoSize = True
+        Me.Label22.Location = New System.Drawing.Point(31, 53)
+        Me.Label22.Name = "Label22"
+        Me.Label22.Size = New System.Drawing.Size(84, 16)
+        Me.Label22.TabIndex = 60
+        Me.Label22.Text = "CAPACIDAD"
         '
         'FrmPrincipal
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.SystemColors.ButtonFace
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Controls.Add(Me.pnlAnyadirVoluntariado)
+        Me.ClientSize = New System.Drawing.Size(994, 614)
+        Me.Controls.Add(Me.Label22)
+        Me.Controls.Add(Me.Label21)
+        Me.Controls.Add(Me.Label20)
+        Me.Controls.Add(Me.Label19)
+        Me.Controls.Add(Me.Label18)
+        Me.Controls.Add(Me.Label17)
+        Me.Controls.Add(Me.Label16)
+        Me.Controls.Add(Me.Label15)
+        Me.Controls.Add(Me.Label14)
+        Me.Controls.Add(Me.Label13)
+        Me.Controls.Add(Me.Label12)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.Label11)
+        Me.Controls.Add(Me.TextBox6)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.TextBox5)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.LBL_NOMBRE_ACTIVIDAD)
         Me.Name = "FrmPrincipal"
         Me.Text = "Gestión Voluntariado"
-        Me.pnlAnyadirVoluntariado.ResumeLayout(False)
-        Me.pnlAnyadirVoluntariado.PerformLayout()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
-    Friend WithEvents pnlAnyadirVoluntariado As Panel
+    Friend WithEvents Label21 As Label
+    Friend WithEvents Label20 As Label
+    Friend WithEvents Label19 As Label
+    Friend WithEvents Label18 As Label
+    Friend WithEvents Label17 As Label
+    Friend WithEvents Label16 As Label
+    Friend WithEvents Label15 As Label
+    Friend WithEvents Label14 As Label
+    Friend WithEvents Label13 As Label
+    Friend WithEvents Label12 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Label11 As Label
+    Friend WithEvents TextBox6 As TextBox
+    Friend WithEvents Label8 As Label
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents ListBox1 As ListBox
     Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents LBL_NOMBRE_ACTIVIDAD As Label
+    Friend WithEvents Label22 As Label
 End Class
